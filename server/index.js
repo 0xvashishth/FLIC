cd98032e416a4645549bbd6da3bdfc82b14dbb80
Vashishth Patel <vashishthchaudhary48@gmail.com>
2023-09-21 19:26:56 +0530

    server models added

--- server/index.js
+++ server/index.js
@@ -2,18 +2,38 @@ require('dotenv').config();
 const express = require('express');
 var cors = require('cors');
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const app = express();
+const connectDB = require("./config/db");
 
 //body-parse
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
+// cookie parser
+app.use(cookieParser())
+
 // cors 
-app.use(cors({ origin: true, credentials: true }));
+var whitelist = ['http://localhost:8082/', "http://localhost:3000/"]
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions));
+// To allow all traffic, use below
+// app.use(cors({ origin: true, credentials: true }));
 
 // Init Middleware
 app.use(express.json({ extended: false }));
 app.use(express.json());
+
+// database connection
+connectDB();
 
 // routers
 
@@ -24,8 +44,7 @@ const middleware = (req, res, next) => {
 }
 
 app.get('/',middleware, (req, res) => {
-  console.log("Hello Flic!!");
-  res.send('API From Flic!!')
+  res.send('Hello Flic!')
 });
 
 const port = process.env.PORT || 8082;
