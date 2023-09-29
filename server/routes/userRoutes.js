4886898efa478d79a6b6efce65f9e0ab762aaad8
Vashishth Patel <76911582+vasu-1@users.noreply.github.com>
2023-09-29 18:17:53 +0000

    getMe and verification middleware added

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,25 +3,25 @@ const express = require("express");
 const router = express.Router();
 const userC = require("../controllers/user-controller");
 const existingUserValidation = require("../middlewares/existingUserValidation")
-const verificationAndBannedCheck = require("../middlewares/verificationAndBannedCheck")
+const { verificationAndBannedCheck } = require("../middlewares/verificationAndBannedCheck")
 
 // Get list of all users
 router.get("/", userC.getAllUser);
 
 // user signup
-router.post("/signup", existingUserValidation , userC.signup);
+router.post("/signup", existingUserValidation, userC.signup);
 
 // User Login
 router.post("/login", loginValiations, userC.login);
 
 // User Update
-router.put("/edit",auth, verificationAndBannedCheck, userC.updateProfile);
+router.put("/edit", auth, verificationAndBannedCheck, userC.updateProfile);
 
 // User Detele All Records
-router.put("/delete",auth, verificationAndBannedCheck,  userC.deleteProfile);
+router.put("/delete", auth, verificationAndBannedCheck, userC.deleteProfile);
 
 // Curently logged in user
-// router.get("/me", protect, userC.getMe);
+router.get("/me", auth, verificationAndBannedCheck, userC.getMe);
 
 // Update user details
 // router.put("/user:id", userC.updateUser);
