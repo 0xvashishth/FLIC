8fe4ffe95ee17ca41662f18bbdeba48b29d732d0
Vashishth Patel <vashishthchaudhary48@gmail.com>
2023-12-20 15:35:06 +0530

    logo and layout configured

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,8 @@
 import NavBar from "./components/NavBar";
-import {opun_black, opun_light, opun_medium} from "./assets/fonts/FontMaster"
 
 export default function Home() {
   return (
-    <main className={`${opun_medium.variable} font-opun-medium`}>
+    <main >
       <NavBar />
     </main>
   );
